3707a808136dc9d9999b0b4f1a62c6f38e116197
Fractaliste <fractaliste@gmail.com>
2021-10-24 10:19:08 +0200

    Début d'implem des lignes de compte

---
+++ src/app/compte/livre/ligne-edition/ligne-edition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'c-ligne-edition',
+  templateUrl: './ligne-edition.component.html',
+  styleUrls: ['./ligne-edition.component.sass']
+})
+export class LigneEditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/compte/livre/ligne/ligne.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'c-ligne',
+  templateUrl: './ligne.component.html',
+  styleUrls: ['./ligne.component.sass']
+})
+export class LigneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
