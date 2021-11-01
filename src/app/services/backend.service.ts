dba8e5067df62aa8a49f202b04e4b8271974d1e4
Fractaliste <fractaliste@gmail.com>
2021-11-01 13:45:47 +0100

    Amélioration du backendService

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,32 +1,77 @@
 import { Injectable } from '@angular/core';
-import { Compte } from '../../../../comptes_api/lib/esm';
+import { Categorie, Compte } from '../../../../comptes_api/lib/esm';
+import { BusService } from './bus/bus.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor() { }
+  constructor(private busService: BusService) { }
 
-  save<T extends object>(entity: T) {
-    const className = entity.constructor.name.toLocaleLowerCase()
-    return fetch("/api/" + className,
+  private _fetch(path: string, method: "GET" | "POST" | "DELETE", body?: object): Promise<any> {
+    const params: RequestInit = { method: method, headers: { "Content-Type": "application/json" } }
+    if (body) {
+      params.body = JSON.stringify(body)
+    }
+
+    return fetch(path, params)
+      .then(res => {
+        if (!res.ok) {
+          console.error("Erreur réseau", res)
+          this.busService.emit(BusService.ErrorMessageEventType, "Erreur réseau lors de l'appel " + path)
+        }
+        return res.json()
+      }).catch(error => {
+        console.error("Erreur applicative", error)
+        this.busService.emit(BusService.ErrorMessageEventType, "Erreur applicative lors de l'appel " + path)
+      }).then(json => {
+        console.debug("Path %s give", path, json);
+        return json
+      })
+  }
+
+  save<T extends object>(entity: T, clazz: { new(): T }): Promise<any>;
+
+  save<T extends object>(entity: T): Promise<any>;
+
+  save<T extends object>(entity: T, clazz?: { new(): T }): Promise<any> {
+    let endpoint: string
+    if (clazz) {
+      endpoint = clazz.name.toLocaleLowerCase()
+    } else {
+      endpoint = entity.constructor.name.toLocaleLowerCase()
+    }
+
+    return fetch("/api/" + endpoint,
       { method: "POST", body: JSON.stringify(entity), headers: { "Content-Type": "application/json" } })
       .then(res => {
-        console.log("res", res);
+        if (!res.ok) {
+          this.busService.emit(BusService.ErrorMessageEventType, "Erreur lors de la sauvegarde")
+        }
+      })
+      .catch((error) => {
+        this.busService.emit(BusService.ErrorMessageEventType, "Erreur lors de la sauvegarde")
       })
   }
 
   getAll<T>(clazz: { new(): T }): Promise<T[]> {
-    return fetch("/api/" + clazz.name.toLocaleLowerCase())
-      .then(res => res.json())
+    return this._fetch("/api/" + clazz.name.toLocaleLowerCase(), "GET")
       .then((jsonArray: T[]) => {
         return jsonArray.map(item => Object.setPrototypeOf(item, clazz.prototype))
       })
-      .then(json => {
-        // console.log("json", json);
-        return json
-      })
+  }
+
+
+  safeDeleteCategorie(categorie: Categorie) {
+    return this._fetch("/api/categorie", "DELETE", categorie)
+  }
+
+  /**
+   * getLignesByCompte
+      */
+  public getLignesByCompte(compte: Compte | number) {
+    return this._fetch("/api/ligne/" + (compte instanceof Compte ? compte.id : compte), "GET")
   }
 
 }
